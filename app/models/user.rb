8edfdacb393c9abbf5df5abb74bf7925b08832d1
Nicholas Page <npage85@gmail.com>
2017-08-15 15:26:01 -0700

    Added admin field to users and admin email evironment variable list.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
       user.image = auth.info.image
+      user.admin = ENV['FB_PHOTO_SITE_ADMINS'].downcase.split(',').include? auth.info.email.downcase
     end
   end
 

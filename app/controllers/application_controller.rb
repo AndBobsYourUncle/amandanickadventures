2470af1a2ad0686111a2a8ab9dceb6fe17d35e41
Nicholas Page <npage85@gmail.com>
2017-08-15 19:28:42 -0700

    Redirect to logout path on FB authentication error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  rescue_from Koala::Facebook::AuthenticationError, with: :user_not_authorized
+
+  def user_not_authorized _auth_error = nil
+    flash[:alert] = 'You need to login with Facebook to continue.'
+
+    redirect_to destroy_user_session_path
+  end
+
   def new_session_path(_scope)
     new_user_session_path
   end

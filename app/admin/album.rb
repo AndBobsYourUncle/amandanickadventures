b5c817e61030f720479f72f5bae1fea298dfa9a6
Nicholas Page <npage85@gmail.com>
2017-08-16 17:44:50 -0700

    Added album model to app.

---
+++ app/admin/album.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Album do
+  # menu parent: 'Albums', label: 'Job Titles', priority: 3
+
+  permit_params :name
+
+  index do
+    column 'Edit' do |job|
+      link_to 'Edit', edit_admin_album_path(job)
+    end
+
+    column :name
+  end
+
+  csv do
+    column :name
+  end
+
+  filter :name
+
+  form do |f|
+    actions
+
+    inputs 'Details' do
+      f.input :name, minimum_input_length: 1
+    end
+  end
+end

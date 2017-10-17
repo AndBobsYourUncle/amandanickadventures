a066f2685ac1b28372f913925269f2af2da81a99
Nicholas Page <npage85@gmail.com>
2017-10-16 23:52:49 -0700

    Get test to build.

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,12 @@ module FacebookPhotoSite
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       storage: :s3,
-      s3_host_name: ENV.fetch('AWS_HOST_NAME'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      s3_host_name: ENV['AWS_HOST_NAME'],
+      s3_region: ENV['AWS_REGION'],
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
-        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        bucket: ENV['S3_BUCKET_NAME'],
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       }
     }
   end
